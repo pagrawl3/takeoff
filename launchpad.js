4d2a272c32dcd8b83647ef8fe41601326ebc230b
Pratham Agrawal <mail@prath.am>
2014-10-16 19:17:39 -0500

    Launchpad application MVP

--- launchpad.js
+++ launchpad.js
@@ -2,29 +2,41 @@
 
 var gulp 	= require('gulp');
 var git  	= require('gulp-git');
+var install	= require('gulp-install');
+var del 	= require('del');
 
 module.exports = function() {
 	// Run git init
 	// src is the root folder for git to initialize
 	gulp.task('init', function(){
-		git.init(function (err) {
+		return git.init(function (err) {
 			if (err) throw err;
 		});
 	});
 
 	gulp.task('clone', function(){
-		git.clone('https://github.com/pagrawl3/launchpad.git', function (err) {
+		return git.clone('https://github.com/pagrawl3/launchpad.git', function (err) {
 			if (err) throw err;
-			console.log("HERE");
-			gulp.start('copy');
-		});
+			gulp.start('install');
+		})
+
 	});
 
 	gulp.task('copy', function(){
-		console.log("COPYING", process.cwd()+'/'+'launchpad/templates/koa/**/*');
-		gulp.src('./launchpad/templates/koa/**/*', { base: process.cwd() })
-		    .pipe(gulp.dest('build/'));
+		return gulp.src(process.cwd()+'/launchpad/templates/koa/**/*')
+		.pipe(gulp.dest('.'))
+		
 	});
+
+	gulp.task('clean', ['copy'], function(cb) {
+		return del(['./launchpad'], cb);
+	})
+
+	gulp.task('install', ['copy', 'clean'], function() {
+		return gulp.src(['./bower.json', './package.json'])
+		.pipe(install());
+	})
+
 
 	gulp.task('default', ['init', 'clone']);
 	gulp.start('default');
